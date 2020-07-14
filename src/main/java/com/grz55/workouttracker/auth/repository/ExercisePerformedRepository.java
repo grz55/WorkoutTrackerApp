9a40f38c0e7bc16032a8e1c05413779fcb0751fc
Grz55 <grz55@vp.pl>
2020-07-14 10:42:19 +0200

    wrong auth folder removed hibernate creation strategy changed

--- src/main/java/com/grz55/workouttracker/auth/repository/ExercisePerformedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grz55.workouttracker.auth.repository;
-
-import com.grz55.workouttracker.auth.model.ExercisePerformed;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExercisePerformedRepository extends JpaRepository<ExercisePerformed,Long> {
-}
--- src/main/java/com/grz55/workouttracker/auth/service/SecurityService.java
+++
@@ -1,8 +0,0 @@
-package com.grz55.workouttracker.auth.service;
-
-public interface SecurityService {
-
-    String findLoggedInUsername();
-
-    void autoLogin(String username, String password);
-}
